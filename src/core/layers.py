9fe74daa3687cac5b605b27e81dbbacf88ca25da
olnkravchenko <alenakravchenko1@gmail.com>
2024-10-31 12:02:11 +0200

    add basic layers

---
+++ src/core/layers.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+def affine_forward(x, w, b):
+    """
+    Computes the forward pass for an affine (fully-connected) layer.
+
+    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    then transform it to an output vector of dimension M.
+
+    Inputs:
+    - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    - w: A numpy array of weights, of shape (D, M)
+    - b: A numpy array of biases, of shape (M,)
+
+    Returns a tuple of:
+    - out: output, of shape (N, M)
+    - cache: (x, w, b)
+    """
+    N = x.shape[0]
+    D = np.prod(x.shape[1:])
+
+    transformed = x.reshape(N, D)
+    out = transformed.dot(w) + b.reshape(1, -1)
+    cache = (x, w, b)
+    return out, cache
+
+
+def affine_backward(dout: np.ndarray, cache):
+    """
+    Computes the backward pass for an affine layer.
+
+    Inputs:
+    - dout: Upstream derivative, of shape (N, M)
+    - cache: Tuple of:
+      - x: Input data, of shape (N, d_1, ... d_k)
+      - w: Weights, of shape (D, M)
+      - b: Biases, of shape (M,)
+
+    Returns a tuple of:
+    - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    - dw: Gradient with respect to w, of shape (D, M)
+    - db: Gradient with respect to b, of shape (M,)
+    """
+    x, w, _ = cache
+    N = x.shape[0]
+    D = np.prod(x.shape[1:])
+
+    transformed = x.reshape(N, D)
+
+    dx = dout.dot(w.T).reshape(x.shape)
+    dw = transformed.T.dot(dout)
+    db = dout.sum(axis=0)
+    return dx, dw, db
+
+
+def relu_forward(x):
+    """
+    Computes the forward pass for a layer of rectified linear units (ReLUs).
+
+    Input:
+    - x: Inputs, of any shape
+
+    Returns a tuple of:
+    - out: Output, of the same shape as x
+    - cache: x
+    """
+    out = np.maximum(0, x)
+    cache = x
+    return out, cache
+
+
+def relu_backward(dout, cache):
+    """
+    Computes the backward pass for a layer of rectified linear units (ReLUs).
+
+    Input:
+    - dout: Upstream derivatives, of any shape
+    - cache: Input x, of same shape as dout
+
+    Returns:
+    - dx: Gradient with respect to x
+    """
+    dx = dout * (cache > 0)
+    return dx
